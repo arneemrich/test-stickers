d3bbf30da568cd407e5bde258c97b563f0cef9f3
CORP\aemrich <arne.emrich@capgemini.com>
2024-03-12 10:03:25 +0100

    fulfull linting rules

--- client-config-app/src/utilities/isAdmin.ts
+++ client-config-app/src/utilities/isAdmin.ts
@@ -3,14 +3,9 @@ export function IsAdmin(idToken: string) {
         // https://learn.microsoft.com/en-us/azure/active-directory/roles/permissions-reference#role-template-ids
         // keep it the same with the server side
         // https://github.com/NewFuture/my-stickers/blob/v2/server.net/Program.cs#L47
-        const directoryRoleTemplateIds = [
-            "62e90394-69f5-4237-9190-012177145e10", // Global Administrator
-            "69091246-20e8-4a56-aa4d-066075b2a7a8", // Teams Administrator
-            "29232cdf-9323-42fd-ade2-1d097af3e4de", // Exchange Administrator
-            "2b745bdf-0803-4d80-aa65-822c4493daac", // Office Apps Administrator
-        ];
+
         const decode = parseJwt(idToken);
-        return decode.scp == "Admin"
+        return decode.scp === "Admin"
     } catch (e) {
         return false;
     }
